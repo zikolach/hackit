c91cb3a707c1f5a9c2a28dbec09c4ec620ea7e4e
Nikolay Kushin <nikolay@indoo.rs>
2016-02-20 16:08:47 +0100

    - highlight tile - prepare for scoreboard

--- frontend/src/main/scala/hackit/GameMap.scala
+++ frontend/src/main/scala/hackit/GameMap.scala
@@ -28,12 +28,18 @@ case class GameMap(canvas: HTMLCanvasElement, width: Int, height: Int) {
     name -> img
   } toMap
 
+  val highlightHex = {
+    val img = createElement("img").asInstanceOf[HTMLImageElement]
+    img.src = s"img/trans.png"
+    img
+  }
+
   val terrains: Map[(Int, Int), String] = generateTerrain
+  var highlightedTile: Option[(Int, Int)] = None
 
   var mouseDown: Option[(Double, Int, Int)] = None
 
   canvas.onmousedown = (e: MouseEvent) => {
-    println(s" ${e.clientX.toInt}, ${e.clientY.toInt}")
     mouseDown = Some((e.timeStamp, e.clientX.toInt, e.clientY.toInt))
   }
 
@@ -42,14 +48,24 @@ case class GameMap(canvas: HTMLCanvasElement, width: Int, height: Int) {
       case Some((timeStamp, x, y)) =>
         println("drag")
         offset = (offset._1 + (e.clientX - x), offset._2 + (e.clientY - y))
-        mouseDown = Some((e.timeStamp, e.clientX.toInt, e.clientY.toInt))
+        mouseDown = Some((timeStamp, e.clientX.toInt, e.clientY.toInt))
       case _ =>
     }
+    val tilePos = calcHex((e.clientX.toInt, e.clientY.toInt))
+    highlightedTile = Some(tilePos)
+    println(highlightedTile)
   }
 
   canvas.onmouseup = (e: MouseEvent) => {
+    mouseDown match {
+      case Some((timeStamp, x, y)) if e.timeStamp - timeStamp < 500 && e.clientX - x < 5 && e.clientY - y < 5 =>
+        println(s"click ${e.clientX}:${e.clientY}")
+      case _ =>
+    }
     mouseDown = None
   }
+
+  canvas.onmouseleave = canvas.onmouseup
 
   private def generateTerrain: Map[(Int, Int), String] = {
     (for {
@@ -59,10 +75,18 @@ case class GameMap(canvas: HTMLCanvasElement, width: Int, height: Int) {
     } yield (x, y) -> kind).toMap
   }
 
+  def drawScoreboard(ctx: Dynamic): Unit = {
+    val centerX = canvas.width / 2
+    val panelW = 200
+    val panelH = 64
+    ctx.fillRect(centerX - panelW / 2, 0, panelW, panelH)
+  }
+
   def render(millis: Double): Unit = {
     val ctx = canvas.getContext("2d")
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     drawTerrain(ctx)
+    drawScoreboard(ctx)
   }
 
   def update(dt: Double) = {
@@ -80,10 +104,24 @@ case class GameMap(canvas: HTMLCanvasElement, width: Int, height: Int) {
     }
   }
 
+  private def calcHex(pos: (Int, Int)): (Int, Int) = {
+    val x = ((pos._1 - offset._1) / (tileSize._1 - tileOffset)).toInt
+    val y = ((pos._2 - offset._2 - (if (x % 2 == 0) tileSize._2 / 2 else 0)) / tileSize._2).toInt
+    (x, y)
+  }
+
   private def drawTerrain(ctx: Dynamic): Unit = {
     terrains.foreach { tile =>
       val pos = calcOffset(tile._1)
+      val Pos = tile._1
       ctx.drawImage(textures(tile._2), pos._1, pos._2)
+      highlightedTile match {
+        case Some(Pos) =>
+          ctx.drawImage(highlightHex, pos._1, pos._2)
+        case _ =>
+
+      }
+
     }
   }
 }
